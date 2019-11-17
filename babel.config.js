8408e879745b048fa7c0b097fc24bc9a169b505f
AndysWork <smlgourab@gmail.com>
2019-11-18 03:23:38 +0530

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  resolve: {
-    alias: {
-      // Alias for using source of BootstrapVue
-      'bootstrap-vue$': 'bootstrap-vue/src/index.js'
-    }
-  },
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  use: ['vue-style-loader', 'css-loader']
+  ]
 }
