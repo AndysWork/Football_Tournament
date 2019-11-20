d240ba9c383f435969270e0013983389f4e658c5
AndysWork <smlgourab@gmail.com>
2019-11-21 00:45:16 +0530

    Updated Table

--- src/components/PointsTable.vue
+++ src/components/PointsTable.vue
@@ -10,10 +10,12 @@ export default {
   data() {
     return {
       items: [
-        { Team: "TBD", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
-        { Team: "TBD", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
-        { Team: "TBD", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
-        { Team: "TBD", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" }
+        { Team: "Platform Footballers", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "WEREWOLVES FC", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "ISES Blasters", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "Sporting FC", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "All has bad knees", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "PTS Panthers", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" }
       ]
     };
   }
