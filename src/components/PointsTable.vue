0b4eb53afa1dc65d318df66034c43b8088515fd6
Gourab Paul <smlgourab@gmail.com>
2019-11-27 15:27:06 +0530

    Updated Team List

--- src/components/PointsTable.vue
+++ src/components/PointsTable.vue
@@ -15,7 +15,9 @@ export default {
         { Team: "ISES Blasters", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
         { Team: "Sporting FC", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
         { Team: "All has bad knees", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
-        { Team: "PTS Panthers", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" }
+        { Team: "PTS Panthers", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "FMS Rangers", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" },
+        { Team: "Vandals", GP: "0", Points: "0", Win: "0", Draw:"0", Lost:"0", GS:"0", GC:"0", GD:"0" }
       ]
     };
   }
