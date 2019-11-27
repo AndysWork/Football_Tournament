11e612b7ab461ebd8b628585ff118ccf0a175530
AndysWork <smlgourab@gmail.com>
2019-11-28 01:52:24 +0530

    Disabled Unused Tab

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,8 +4,8 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="nav--height">
-        <b-nav-item href="#">News</b-nav-item>
-        <b-nav-item href="#">Gallery</b-nav-item>
+        <!-- <b-nav-item href="#">News</b-nav-item>
+        <b-nav-item href="#">Gallery</b-nav-item> -->
         <b-nav-item href="/tournament">Futsal 1.0</b-nav-item>
       </b-navbar-nav>
 
